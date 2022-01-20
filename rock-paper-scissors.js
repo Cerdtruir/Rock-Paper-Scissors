5f862596dbb05e5c37f7c3704cff8b60a19edeba
Cerdtruir <ghoornabeel@gmail.com>
2022-01-20 14:15:30 +0200

    add numbers to corrspond to hands in compterplay

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,16 @@
 function computerPlay() {
- return Math.floor(Math.random() * 3);
+  let computerHand = Math.floor(Math.random() * 3);
+  if (computerHand === 0) {
+    return "rock";
+
+  }
+  else if (computerHand === 1) {
+    return "paper";
+
+  }
+  else if (computerHand === 2) {
+    return "scissors";
+
+  }
 }
+console.log(computerPlay());
