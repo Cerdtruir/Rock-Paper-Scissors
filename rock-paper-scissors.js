96b1a28ab03d7d0e05cba2dec2b52a610b7f635a
Cerdtruir <ghoornabeel@gmail.com>
2022-01-20 14:47:20 +0200

    add play round function with every iteration

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,3 +14,44 @@ function computerPlay() {
   }
 }
 console.log(computerPlay());
+
+
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+    if (computerSelection === "rock") {
+      return `It's A Draw! You Both Chose ${playerSelection}`
+    }
+    else if (computerSelection === "paper") {
+      return `You Lose! ${computerSelection} Beats ${playerSelection}`
+    }
+    else if (computerSelection === "scissors") {
+      return `You Win! ${playerSelection} Beats ${computerSelection}`
+    }
+  }
+  else if (playerSelection === "paper") {
+    if (computerSelection === "rock") {
+      return `You Win! ${playerSelection} Beats ${computerSelection}`
+    }
+    else if (computerSelection === "paper") {
+      return `It's A Draw! You Both Chose ${playerSelection}`
+    }
+    else if (computerSelection === "scissors") {
+      return `You Lose! ${computerSelection} Beats ${playerSelection}`
+    }
+  }
+  else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return `You Lose! ${computerSelection} Beats ${playerSelection}`
+    }
+    else if (computerSelection === "paper") {
+      return `You Win! ${playerSelection} Beats ${computerSelection}`
+    }
+    else if (computerSelection === "scissors") {
+      return `It's A Draw! You Both Chose ${playerSelection}`
+    }
+  }
+}
+console.log(playRound("rock", computerPlay()));
+//playerSelection = prompt ("Pick a hand");
+//playerSelection.toLowerCase();
